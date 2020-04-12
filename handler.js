ef560759953f9b44b594d1f45e92a3de4a01a89c
gopal-g <c4gopal@gmail.com>
2020-04-12 18:20:29 +0530

    - Commit #4

--- handler.js
+++ handler.js
@@ -8,7 +8,7 @@ module.exports.helloWorld = (event, context, callback) => {
       'Access-Control-Allow-Origin': '*',
     },
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully',
+      message: 'Go Serverless v1.0! Your function executed successfully!!',
       input: event,
     }),
   };
