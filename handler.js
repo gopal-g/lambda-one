89ad2c2b9baed48be878578cc8a2aae2fa1d6597
Gopal <gopal.g@enchanting-travels.com>
2020-04-12 18:16:16 +0530

    - commit #3

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ module.exports.helloWorld = (event, context, callback) => {
   const response = {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+      'Access-Control-Allow-Origin': '*',
     },
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully',
